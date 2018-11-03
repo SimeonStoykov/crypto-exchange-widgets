4aab2d886c078ca1e7c4171db695d96daceff7d9
Simeon Petkov Stoykov <simeon_stoykov@mail.bg>
2018-11-03 13:06:41 +0200

    Updates

---
+++ precache-manifest.6b9f4f36883a8eeb1950fbba44f686f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ef426328ce635462b5b",
+    "url": "/crypto-exchange-widgets/static/js/runtime~main.1ef42632.js"
+  },
+  {
+    "revision": "65bc755233ef5502240e",
+    "url": "/crypto-exchange-widgets/static/js/main.65bc7552.chunk.js"
+  },
+  {
+    "revision": "46ae314c19678fe8970e",
+    "url": "/crypto-exchange-widgets/static/js/1.46ae314c.chunk.js"
+  },
+  {
+    "revision": "65bc755233ef5502240e",
+    "url": "/crypto-exchange-widgets/static/css/main.32a2e765.chunk.css"
+  },
+  {
+    "revision": "2db64e792f82b71d5dc81911a000619c",
+    "url": "/crypto-exchange-widgets/index.html"
+  }
+];
